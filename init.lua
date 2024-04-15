67bd8e49b5854665a93ee9fa62e21e8a2edce309
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-04-15 15:58:31 -0400

    vim kitty

--- init.lua
+++ init.lua
@@ -327,6 +327,10 @@ require('lazy').setup({
     "mfussenegger/nvim-jdtls",
   },
 
+  -- kitty.conf highlighting
+  {
+    "fladson/vim-kitty"
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
