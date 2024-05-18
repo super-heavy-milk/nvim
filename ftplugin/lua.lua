b37d32e7a0e99db722be79bdf040f11324afe686
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-05-18 16:49:44 -0400

    Run commands, lua

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,6 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.expandtab = true
+
+vim.keymap.set('n', '<leader>x', '<cmd>source %<CR>', { desc = 'Execute the current file' })
+vim.keymap.set('n', '<leader>X', '<cmd>.lua<CR>', { desc = 'Execute the current line' })
