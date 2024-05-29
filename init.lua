63995dae9731789f18d6259dd01b45ecda2176d3
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-05-29 10:10:00 -0400

    Trying rose pine for a bit

--- init.lua
+++ init.lua
@@ -145,19 +145,33 @@ require('lazy').setup({
         },
     },
 
+    -- {
+    --     'briones-gabriel/darcula-solid.nvim',
+    --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --     priority = 1000, -- make sure to load this before all the other start plugins
+    --     dependencies = {
+    --         'rktjmp/lush.nvim',
+    --     },
+    --     config = function()
+    --         -- vim.cmd.colorscheme 'darcula-solid'
+    --         -- -- below lines make the background inherit the terminal background
+    --         -- vim.cmd 'hi Normal ctermbg=none'
+    --         -- vim.cmd 'highlight NonText ctermbg=none'
+    --         -- vim.cmd 'hi Normal guibg=none'
+    --     end,
+    -- },
+
     {
-        'briones-gabriel/darcula-solid.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        dependencies = {
-            'rktjmp/lush.nvim',
-        },
+        'rose-pine/neovim',
+        name = 'rose-pine',
         config = function()
-            vim.cmd.colorscheme 'darcula-solid'
-            -- below lines make the background inherit the terminal background
-            vim.cmd 'hi Normal ctermbg=none'
-            vim.cmd 'highlight NonText ctermbg=none'
-            vim.cmd 'hi Normal guibg=none'
+            require('rose-pine').setup {
+                dim_inactive_windows = true,
+                styles = {
+                    transparency = true,
+                },
+            }
+            vim.cmd.colorscheme 'rose-pine-moon'
         end,
     },
 
