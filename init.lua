ca8cfab9afadb4cde03c2073a808f1cb46f872a0
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-05-29 11:54:39 -0400

    clean up some comments

--- init.lua
+++ init.lua
@@ -476,13 +476,12 @@ require('lazy').setup({
     -- require 'kickstart.plugins.autoformat',
     -- require 'kickstart.plugins.debug',
 
-    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-    --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-
-    --    up-to-date with whatever is in the kickstart repo.
-    --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-    --
-    --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+    -- NOTE: The import below can automatically add your own plugins, configuration,
+    --       etc from `lua/custom/plugins/*.lua`
+    --    You can use this folder to prevent any conflicts with this init.lua if you're interested
+    --    in keeping up-to-date with whatever is in the kickstart repo.
+    --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to begin.
+    --    For more information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
 
     { import = 'custom.plugins' },
 }, {})
