3268436129948d1614f0c3ce80f7766e1a57b868
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-01 09:28:04 -0400

    Move nvim-lint to it's own file

---
+++ lua/custom/plugins/nvim-lint.lua
@@ -0,0 +1,40 @@
+return {
+    -- linting
+    'mfussenegger/nvim-lint',
+    event = {
+        'BufReadPre',
+        'BufNewFile',
+    },
+    config = function()
+        local lint = require 'lint'
+
+        lint.linters_by_ft = {
+            bash = { 'shellcheck', 'codespell' },
+            css = { 'stylelint' },
+            editorconfig = { 'editorconfig-checker' },
+            html = { 'htmlhint' },
+            java = { 'codespell' },
+            -- javascript = { 'eslint_d' },
+            -- javascriptreact = { 'eslint_d' },
+            -- markdown = { 'proselint', 'codespell' },
+            python = { 'ruff', 'codespell' },
+            sh = { 'shellcheck', 'codespell' },
+            -- svelte = { 'eslint_d' },
+            -- typescript = { 'eslint_d' },
+            -- typescriptreact = { 'eslint_d' },
+            yaml = { 'yamllint', 'codespell' },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+
+        vim.keymap.set('n', '<leader>l', function()
+            lint.try_lint()
+        end, { desc = 'Trigger linting for current file' })
+    end,
+}
