e8eabc918834dbfa7247447e5b7af691ce2de499
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-01 09:21:04 -0400

    Move telescope to it's own file

--- init.lua
+++ init.lua
@@ -30,26 +30,6 @@ vim.opt.rtp:prepend(lazypath)
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-    },
-
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-            return vim.fn.executable 'make' == 1
-        end,
-    },
-
     {
         -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
