8aeb4fb79bf7bb87d52502cbdd77fc83a4c4a2b0
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-02 10:01:33 -0400

    Rose-pine: italic=false, dim_inactive=false

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -25,9 +25,10 @@ return {
         name = 'rose-pine',
         config = function()
             require('rose-pine').setup {
-                dim_inactive_windows = true,
+                dim_inactive_windows = false,
                 styles = {
                     transparency = true,
+                    italic = false,
                 },
             }
             -- vim.cmd.colorscheme 'rose-pine-moon'
