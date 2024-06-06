f2b30515a4a02b0c13f95d49450a3224cfcf01b5
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-06 14:15:06 -0400

    Change execute to 'xl' and 'xf'

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,5 +2,5 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.expandtab = true
 
-vim.keymap.set('n', '<leader>x', '<cmd>w | source %<CR>', { desc = 'Execute the current file' })
-vim.keymap.set('n', '<leader>X', '<cmd>.lua<CR>', { desc = 'Execute the current line' })
+vim.keymap.set('n', '<leader>xf', '<cmd>w | source %<CR>', { desc = 'Execute the current file' })
+vim.keymap.set('n', '<leader>xl', '<cmd>.lua<CR>', { desc = 'Execute the current line' })
