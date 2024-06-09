161bfb9e05262bd60b08d3b96a38cbfc16181300
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-09 17:43:22 -0400

    Add some settings for terminal mode

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -27,3 +27,26 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
     -- this is nice, but will mess with copy/paste
     --command = "setlocal spell wrap linebreak autoindent formatoptions=tacqw textwidth=80 wrapmargin=0",
 })
+
+local term_group = vim.api.nvim_create_augroup('term_group', { clear = true })
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+    group = term_group,
+    callback = function()
+        -- make escape work in the terminal properly
+        vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
+
+        -- terminal mode seems to only partially respect this?
+        vim.opt_local.guicursor = {
+            'n-v-c:block', -- normal, visual, cmd-line normal
+            'i-ci-ve:ver10', -- insert, cmd-line insert, visual selection
+            'r-cr:hor20', -- replace, cmd-line replace
+            'o:hor50', -- operator-pending
+            'a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor', -- all modes
+            'sm:block-blinkwait175-blinkoff150-blinkon175', -- show-match in insert mode
+        }
+
+        vim.opt_local.spell = false
+        vim.opt_local.number = false
+        vim.cmd.startinsert()
+    end,
+})
